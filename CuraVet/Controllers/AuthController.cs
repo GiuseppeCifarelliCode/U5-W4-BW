0af7f023742ed6593726d51bd6aa3bc4a4c0ae68
Giuseppe Cifarelli <cifarelli1995@gmail.com>
2023-10-16 12:04:59 +0200

    Added Auth and Login

---
+++ CuraVet/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using CuraVet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace CuraVet.Controllers
+{
+    public class AuthController : Controller
+    {
+        private ModelDBContext db = new ModelDBContext();
+        // GET: Auth
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            User u = db.User.SingleOrDefault(x => x.Username == user.Username);
+            if (u.Username != null && u.Password == user.Password)
+            {
+                FormsAuthentication.SetAuthCookie(user.Username, false);
+                if (u.Ruolo == "Vet") return RedirectToAction("Index", "Clinica");
+                else return RedirectToAction("Index", "Farmacia");
+            }
+            else
+            {
+                ViewBag.Error("Username e Password non coincidono!");
+                return View();
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
